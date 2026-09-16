9bb4e107f9db3f9dd43978252cb0f789ffbe2860
Leonardo Daniel <leonardodand245@gmail.com>
2026-09-16 00:32:35 -0300

    correção do host local

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://98.91.20.39:1026',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
